a8cf854991da1dbefe42c0e049d39098eb34de81
Winnie Luk <winniecluk@gmail.com>
2017-07-07 22:36:55 -0700

    add topbar layout

--- index.ios.js
+++ index.ios.js
@@ -13,6 +13,9 @@ import {
 } from 'react-native';
 import MapView from 'react-native-maps';
 
+const LATITUDE_DELTA = 0.5, LONGITUDE_DELTA = 0.45, LATITUDE_DELTA_ZOOM = 0.02, LONGITUDE_DELTA_ZOOM = 0.015
+
+
 export default class letsGoMetro extends Component {
   constructor(props){
     super(props);
@@ -20,8 +23,8 @@ export default class letsGoMetro extends Component {
       region: {
         latitude: 34.0498,
         longitude: -118.2389,
-        latitudeDelta: 0.5,
-        longitudeDelta: 0.45
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
       }
     };
   }
@@ -29,31 +32,57 @@ export default class letsGoMetro extends Component {
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
       console.log(position);
+      this.setState({
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDE_DELTA_ZOOM,
+          longitudeDelta: LONGITUDE_DELTA_ZOOM
+        }
+      })
     })
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          region={this.state.region}>
-        </MapView>
+      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-start'}}>
+        <View style={styles.information}>
+        </View>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            region={this.state.region}>
+          </MapView>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  information: {
+    // ...StyleSheet.absoluteFillObject,
+    flex: 0.5,
+    // height: 180,
+    alignItems: 'stretch',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: 'red',
+  },
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    // ...StyleSheet.absoluteFillObject,
+    flex: 2,
+    // height: 400,
+    // justifyContent: 'flex-end',
+    // alignItems: 'center',
+    alignItems: 'stretch',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: 'blue',
   },
   map: {
-    ...StyleSheet.absoluteFillObject
+    ...StyleSheet.absoluteFillObject,
+    borderColor: 'orange'
   },
   welcome: {
     fontSize: 20,
