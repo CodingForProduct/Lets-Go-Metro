9fa8b6315d6a2397b90593784d2d61bbb32a693f
Winnie Luk <winniecluk@gmail.com>
2017-07-10 21:33:19 -0700

    paste into android

--- index.android.js
+++ index.android.js
@@ -7,51 +7,37 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  View,
+  // StyleSheet,
+  // Text,
+  // View,
+  // TextInput,
+  // TouchableOpacity
 } from 'react-native';
-import MapView from 'react-native-maps';
+// import MapView from 'react-native-maps';
+import Information from './app/components/Information';
+import MetroMap from './app/components/MetroMap';
+import DirectionsBar from './app/components/DirectionsBar';
+
 
 export default class letsGoMetro extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          region={{
-          latitude: 34.0498,
-          longitude: -118.2389,
-          latitudeDelta: 0.5,
-          longitudeDelta: 0.45
-        }}>
-        </MapView>
+      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'stretch'}}>
+        <Information />
+        <MetroMap />
+        <DirectionsBar />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+// });
 
 AppRegistry.registerComponent('letsGoMetro', () => letsGoMetro);
