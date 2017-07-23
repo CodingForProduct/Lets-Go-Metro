fc91a89847657a13b508d6e991937c1d80a25976
Winnie Luk <winniecluk@gmail.com>
2017-07-23 10:37:01 -0700

    clean up code

--- app/components/DirectionsBar.js
+++ app/components/DirectionsBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import helper from '../utils/directionsHelper';
-const decodePolyline = require('decode-google-map-polyline');
 import {
   View,
   TextInput,
@@ -25,14 +24,19 @@ export default class DirectionsBar extends Component {
   constructor(props){
     super(props);
     this.state = {
-      originSelectionAddress: "",
-      originSelectionCoord: {
+      currentPosition: {
         latitude: "",
         longitude: ""
       },
+      originText: "",
       destinationText: "",
+      suggestion1: "",
+      suggestion2: "",
+      originSuggestion1: "",
+      originSuggestion2: "",
       destinationSuggestion1: "",
       destinationSuggestion2: "",
+      originSelection: "",
       destinationSelection: "",
       showPredictions: false,
       showInput: true,
@@ -43,12 +47,15 @@ export default class DirectionsBar extends Component {
       directionsHeight: new Animated.Value(0),
       inputHeight: new Animated.Value(100),
       textBoxHeight: 40,
-      directionsArr: []
+      directionsArr: [],
+      inputFocus: ''
     }
     this.setOriginText = this.setOriginText.bind(this);
-    this.animateBar = this.animateBar.bind(this);
+    this.setDestinationText = this.setDestinationText.bind(this);
     this.chooseAddress1 = this.chooseAddress1.bind(this);
     this.chooseAddress2 = this.chooseAddress2.bind(this);
+    this.setA = this.setA.bind(this);
+    this.setB = this.setB.bind(this);
   }
 
   componentDidMount() {
@@ -57,11 +64,15 @@ export default class DirectionsBar extends Component {
       fetch(endpt, {
         method: 'GET'
       }).then(response => {
+        // console.log(response);
+        // console.log(typeof response._bodyInit);
+
         this.setState({
-          originSelectionAddress: JSON.parse(response._bodyInit).results[0].formatted_address,
-          originSelectionCoord: {
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude
+          // originText: JSON.parse(response._bodyInit).results[0].formatted_address,
+          // originSelection: JSON.parse(response._bodyInit).results[0].formatted_address,
+          currentPosition: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
           }
         });
       }).catch(err => {
@@ -71,116 +82,50 @@ export default class DirectionsBar extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+    // need to fix below later
+    if ((prevState.destinationSelection != this.state.destinationSelection || prevState.originSelection != this.state.originSelection) && (this.state.originSelection && this.state.destinationSelection)){
+      console.log('INSIDE GET ADDRESS IF');
+      var destinationStr = this.state.destinationSelection == 'suggestion1' ? destinationSuggestion1 : destinationSuggestion2;
+      var originStr = this.state.originSelection == 'suggestion1' ? originSuggestion1 : this.state.originSelection == 'suggestion2' ? originSuggestion2 : originText
 
-    // making the call for directions
-    if (prevState.destinationSelection != this.state.destinationSelection && (this.state.destinationSelection && this.state.originSelectionAddress)){
+      console.log('THIS IS DEST' + destinationStr);
+      console.log('THIS IS ORIGIN' + originStr);
+    }
 
-      if (this.state.destinationSelection == 'suggestion1'){
-        var qsDestination = this.state.destinationSuggestion1.split(' ').join('+');
-        var qsOrigin = this.state.originSelectionAddress.split(' ').join('+')
-        let endpt = API_DIRECTIONS_ROOT + '&origin=' + qsOrigin + '&destination=' + qsDestination;
-        console.log('THIS IS THE ENDPT');
-        console.log(endpt);
-
-        fetch(endpt, {
-          method: 'GET'
-        }).then(response => {
-          console.log('THESE ARE DIRECTIONS WE ARE GETTING BACK FROM SERVER');
-          // console.log(response);
-          let responseData = JSON.parse(response._bodyInit);
-          console.log(responseData);
-
-          let legs = responseData.routes[0].legs[0].steps;
-          let stepsArr = [];
-          let directionsArr = [];
-          let transitDetails = [];
-
-          legs.forEach(el => {
-            if (el.travel_mode === 'TRANSIT'){
-              stepsArr.push({
-                latitude: el.start_location.lat,
-                longitude: el.start_location.lng
-              });
-              stepsArr.push({
-                latitude: el.end_location.lat,
-                longitude: el.end_location.lng
-              })
-              // directionsArr.push(el.html_instructions);
-
-              var directionObj = {};
-              directionObj["key"] = el.html_instructions;
-              console.log('THIS IS DIRECTION OBJ INSIDE TRANSIT');
-              console.log(directionObj);
-              directionsArr.push(
-                directionObj
-              );
-              transitDetails.push(el.transit_details);
-            } else if (el.travel_mode === 'WALKING'){
-              // the overview instructions
-              // let directionObj = {};
-              // directionObj["key"] = el.html_instructions;
-              // directionsArr.push(el.html_instructions);
-              el.steps.forEach(step => {
-                stepsArr.push({
-                  latitude: step.start_location.lat,
-                  longitude: step.start_location.lng
-                });
-                stepsArr.push({
-                  latitude: step.end_location.lat,
-                  longitude: step.end_location.lng
-                });
-                if (step.html_instructions){
-                  // directionsArr.push(step.html_instructions);
-
-                  var directionObj = {};
-                  directionObj["key"] = step.html_instructions;
-                  directionsArr.push(
-                    directionObj
-                  );
-                }
-              });
-            }
-          });
-
-          console.log(transitDetails);
-          console.log('THIS IS DIRECTIONS ARR');
-          console.log(directionsArr);
-          this.setState({
-            directionsArr: directionsArr
-          });
-          this.props.updatePolylineCoord(stepsArr);
-
-        });
-
-      } else if (this.state.destinationSelection == 'suggestion2'){
-
-      }
-    } // closes prevState & thisState
-
-
-    // make call for suggestions
+    // make call for suggestions -- change data
     if (prevState.destinationText != this.state.destinationText){
-      // console.log('THIS IS THE CURRENT TEXT');
-      // console.log(this.state.destinationText);
-      // console.log('MAKING A FETCH');
-      var qs = this.state.destinationText.split(' ').join('+');
-
-      fetch(API_PLACES_ROOT + '&input=' + qs, {
-        method: 'GET'
-      }).then((response) => {
-        var predictionsArr = JSON.parse(response._bodyInit).predictions;
+      console.log(this.state.destinationText);
+      helper.getPredictions(this.state.destinationText).then(predictionsArr => {
+        console.log(predictionsArr);
         if (predictionsArr.length > 1){
           this.setState({
             destinationSuggestion1: predictionsArr[0].description,
-            destinationSuggestion2: predictionsArr[1].description
+            destinationSuggestion2: predictionsArr[1].description,
+            suggestion1: predictionsArr[0].description,
+            suggestion2: predictionsArr[1].description
           });
         }
-      }).catch((err) => {
+      }).catch(err => {
         console.log(err);
       });
     }
 
-    // expand bar
+    if (prevState.originText != this.state.originText){
+      helper.getPredictions(this.state.originText).then(predictionsArr => {
+        if (predictionsArr.length > 1){
+          this.setState({
+            originSuggestion1: predictionsArr[0].description,
+            originSuggestion2: predictionsArr[1].description,
+            suggestion1: predictionsArr[0].description,
+            suggestion2: predictionsArr[1].description
+          });
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+
+    // expand bar -- change view
     if (prevState.showPredictions != this.state.showPredictions){
       if (this.state.showPredictions === true){
         this.setState({
@@ -201,8 +146,7 @@ export default class DirectionsBar extends Component {
           }
         ).start();
 
-      }
-      else {
+      } else {
         this.setState({
           noHeightBtn: 0
         });
@@ -242,43 +186,84 @@ export default class DirectionsBar extends Component {
             duration: 500
           }
         ).start();
+      } else {
+          this.setState({
+          textBoxHeight: 40,
+        });
+        Animated.timing(
+          this.state.inputHeight,
+          {
+            toValue: 100,
+            duration: 500
+          }
+        ).start();
+        Animated.timing(
+          this.state.directionsHeight,
+          {
+            toValue: 0,
+            duration: 500
+          }
+        ).start();
       }
     }
   }
 
   setOriginText(text){
     console.log('INSIDE SET ORIGIN TEXT');
-    console.log(text);
     this.setState({
-      destinationText: text
+      originText: text,
+      showPredictions: true,
+      showInput: true,
+      showDirections: false
     });
   }
 
-  animateBar(){
-    console.log('ANIMATEBAR CONSOLE LOG');
+  setDestinationText(text){
+    console.log('INSIDE SET DESTINATION TEXT');
     this.setState({
-      showPredictions: true
-    })
+      destinationText: text,
+      showPredictions: true,
+      showInput: true,
+      showDirections: false
+    });
+  }
+
+  setA(){
+    this.setState({
+      inputFocus: 'origin'
+    });
+  }
+
+  setB(){
+    this.setState({
+      inputFocus: 'destination'
+    });
   }
 
   chooseAddress1(){
     console.log('CHOSE ADDRESS');
-    this.setState({
-      destinationSelection: "suggestion1",
-      showPredictions: false,
-      showDirections: true
-      // showDirections
-      // showInput
-    });
+    if (this.state.inputFocus == 'origin'){
+      this.setState({
+        originSelection: "suggestion1"
+      });
+    } else if (this.state.inputFocus == 'destination'){
+      this.setState({
+        destinationSelection: "suggestion1"
+      });
+    }
   }
 
   chooseAddress2(){
     console.log('CHOSE ADDRESS');
-    this.setState({
-      destinationSelection: "suggestion2",
-      showPredictions: false,
-      showDirections: true
-    });
+    if (this.state.inputFocus == 'origin'){
+      this.setState({
+        originSelection: "suggestion2"
+      });
+    } else if (this.state.inputFocus == 'destination'){
+      this.setState({
+        destinationSelection: "suggestion2"
+      });
+    }
   }
 
   render(){
@@ -305,13 +290,13 @@ export default class DirectionsBar extends Component {
           justifyContent: 'center',
           backgroundColor: '#fff'
         }}>
-          <FlatList data={this.state.directions} renderItem={({el}) =>
+          <FlatList data={this.state.directionsArr} renderItem={({item}) =>
             <Text style={{height: 50,
               borderRadius: 4,
               borderWidth: 0.5,
               borderColor: 'orange',
               zIndex: 200
-            }}>{el.key}</Text>
+            }}>{item.key}</Text>
           }
           />
         </Animated.View>
@@ -334,7 +319,11 @@ export default class DirectionsBar extends Component {
               borderColor: 'black',
               width: 250,
               paddingLeft: 10
-            }} placeholder="Current Location" value={this.state.originSelectionAddress} />
+            }} placeholder="Current Location"
+            value={this.state.originText}
+            onFocus={this.state.setA}
+            onChangeText={this.setOriginText}
+            />
             <TextInput style={{
               height: this.state.textBoxHeight,
               borderRadius: 4,
@@ -342,7 +331,10 @@ export default class DirectionsBar extends Component {
               borderColor: 'black',
               width: 250,
               paddingLeft: 10
-            }} placeholder="Destination" onFocus={this.animateBar} onChangeText={this.setOriginText} />
+            }} placeholder="Destination"
+            onFocus={this.state.setB}
+            onChangeText={this.setDestinationText}
+            />
           </View>
         </Animated.View>
         <Animated.View style={{
@@ -358,18 +350,18 @@ export default class DirectionsBar extends Component {
               }}>
               <View>
                 <Text>
-                  {this.state.destinationSuggestion1}
+                  {this.state.suggestion1}
                 </Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress={this.chooseAddress2} style={{    borderWidth: 0.5,
+            <TouchableOpacity onPress={this.chooseAddress2} style={{borderWidth: 0.5,
                   borderColor: 'purple',
                   justifyContent: 'center',
                   height: this.state.noHeightBtn
               }}>
               <View>
                 <Text>
-                  {this.state.destinationSuggestion2}
+                  {this.state.suggestion2}
                 </Text>
               </View>
             </TouchableOpacity>
@@ -428,3 +420,92 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
   },
 });
+
+    // making the call for directions
+
+    // if (prevState.destinationSelection != this.state.destinationSelection && (this.state.destinationSelection && this.state.originText)){
+
+    //   if (this.state.destinationSelection == 'suggestion1'){
+    //     var qsDestination = this.state.destinationSuggestion1.split(' ').join('+');
+    //     var qsOrigin = this.state.originText.split(' ').join('+');
+    //     let endpt = API_DIRECTIONS_ROOT + '&origin=' + qsOrigin + '&destination=' + qsDestination;
+    //     console.log('THIS IS THE ENDPT');
+    //     console.log(endpt);
+
+    //     fetch(endpt, {
+    //       method: 'GET'
+    //     }).then(response => {
+    //       console.log('THESE ARE DIRECTIONS WE ARE GETTING BACK FROM SERVER');
+    //       // console.log(response);
+    //       let responseData = JSON.parse(response._bodyInit);
+    //       console.log(responseData);
+
+    //       let legs = responseData.routes[0].legs[0].steps;
+    //       let stepsArr = [];
+    //       let directionsArr = [];
+    //       let transitDetails = [];
+
+    //       legs.forEach(el => {
+    //         if (el.travel_mode === 'TRANSIT'){
+    //           stepsArr.push({
+    //             latitude: el.start_location.lat,
+    //             longitude: el.start_location.lng
+    //           });
+    //           stepsArr.push({
+    //             latitude: el.end_location.lat,
+    //             longitude: el.end_location.lng
+    //           })
+    //           // directionsArr.push(el.html_instructions);
+
+    //           var directionObj = {};
+    //           directionObj["key"] = el.html_instructions;
+    //           console.log('THIS IS DIRECTION OBJ INSIDE TRANSIT');
+    //           console.log(directionObj);
+    //           directionsArr.push(
+    //             directionObj
+    //           );
+    //           transitDetails.push(el.transit_details);
+    //         } else if (el.travel_mode === 'WALKING'){
+    //           // the overview instructions
+    //           // let directionObj = {};
+    //           // directionObj["key"] = el.html_instructions;
+    //           // directionsArr.push(el.html_instructions);
+    //           el.steps.forEach(step => {
+    //             stepsArr.push({
+    //               latitude: step.start_location.lat,
+    //               longitude: step.start_location.lng
+    //             });
+    //             stepsArr.push({
+    //               latitude: step.end_location.lat,
+    //               longitude: step.end_location.lng
+    //             });
+    //             if (step.html_instructions){
+    //               // directionsArr.push(step.html_instructions);
+
+    //               var directionObj = {};
+    //               directionObj["key"] = step.html_instructions;
+    //               directionsArr.push(
+    //                 directionObj
+    //               );
+    //             }
+    //           });
+    //         }
+    //           this.setState({
+    //             directionsArr: directionsArr
+    //           });
+    //       });
+
+    //       console.log(transitDetails);
+    //       console.log('THIS IS DIRECTIONS ARR');
+    //       console.log(directionsArr);
+    //       this.setState({
+    //         directionsArr: directionsArr
+    //       });
+    //       this.props.updatePolylineCoord(stepsArr);
+
+    //     });
+
+    //   } else if (this.state.destinationSelection == 'suggestion2'){
+
+    //   }
+    // } // closes prevState & thisState
--- app/utils/directionsHelper.js
+++ app/utils/directionsHelper.js
@@ -6,8 +6,77 @@ const API_KEY = 'AIzaSyCdPnAPE-Kqy_VWKiFtX8Zm4b0T7wyyZ38',
 
 
 var helpers = {
+  getDirections: function(selectText){
+    var qsDestination = selectText.split(' ').join('+');
+    var qsOrigin = selectText.split(' ').join('+');
+    let endpt = API_DIRECTIONS_ROOT + '&origin=' + qsOrigin + '&destination=' + qsDestination;
+    fetch(endpt, {
+      method: 'GET'
+    }).then(response => {
+      let responseData = JSON.parse(response._bodyInit);
+      let legs = responseData.routes[0].legs[0].steps;
+      let stepsArr = [];
+      let directionsArr = [];
+      let transitDetails = [];
 
-}
+      legs.forEach(el => {
+        if (el.travel_mode === 'TRANSIT'){
+          stepsArr.push({
+            latitude: el.start_location.lat,
+            longitude: el.start_location.lng
+          });
+          stepsArr.push({
+            latitude: el.end_location.lat,
+            longitude: el.end_location.lng
+          });
+
+          var directionObj = {};
+          directionObj["key"] = el.html_instructions;
+          directionsArr.push(
+            directionObj
+          );
+          transitDetails.push(el.transit_details);
+        }
+
+        // else if
+        else if (el.travel_mode === 'WALKING'){
+          el.steps.forEach(step => {
+            stepsArr.push({
+              latitude: step.start_location.lat,
+              longitude: step.start_location.lng
+            });
+            stepsArr.push({
+              latitude: step.end_location.lat,
+              longitude: step.end_location.lng
+            });
+            if (step.html_instructions){
+              var directionObj = {};
+              directionObj["key"] = step.html_instructions;
+              directionsArr.push(directionObj);
+            }
+          });
+        } // closes else if
+        return directionsArr;
+        // this.setState({
+        //   directionsArr: directionsArr
+        // }); // closes setState
+      }); // closes legs forEach
+    }); // closes then for fetch
+
+
+  }, // closes getDirections
+
+  getPredictions: function(selectText){
+    var qs = selectText.split(' ').join('+');
+    return fetch(API_PLACES_ROOT + '&input=' + qs, {
+      method: 'GET'
+    }).then((response) => {
+      var predictionsArr = JSON.parse(response._bodyInit).predictions;
+      return predictionsArr;
+    });
+  }
+
+} // closes helpers
 
 
 module.exports = helpers;
